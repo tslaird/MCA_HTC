0fd22f8e6dcd243947769e543e642c90bc32b30f
Tyler Laird <tslaird@login05.osgconnect.net>
2022-07-20 14:59:35 -0500

    edited files

--- BacFilt.R
+++ BacFilt.R
@@ -2,6 +2,8 @@ library(dada2); packageVersion("dada2")
 # File parsing
 pathF <- sort(list.files(pattern = "R1_001.fastq", full.names = TRUE))
 pathR <- sort(list.files(pattern = "R2_001.fastq", full.names = TRUE))
+print(pathF)
+print(pathR)
 #pathF <- "MiSeq_SOP/FWD" # CHANGE ME to the directory containing your demultiplexed forward-read fastqs
 #pathR <- "MiSeq_SOP/REV" # CHANGE ME ...
 #filtpathF <- file.path(pathF, "filtered") # Filtered forward files go into the pathF/filtered/ subdirectory
@@ -14,7 +16,7 @@ if(length(fastqFs) != length(fastqRs)) stop("Forward and reverse files do not ma
 #              rev=file.path(pathR, fastqRs), filt.rev=file.path(filtpathR, fastqRs),
 #              truncLen=c(240,200), maxEE=2, truncQ=11, maxN=0, rm.phix=TRUE,
 #              compress=TRUE, verbose=TRUE, multithread=TRUE)
-filterAndTrim(fwd=file.path(pathF, fastqFs), filt=paste0("filtered_",pathF),
-              rev=file.path(pathR, fastqRs), filt.rev=paste0("filtered_",pathR),
+filterAndTrim(fwd=pathF , filt=paste0("filtered_",pathF),
+              rev=pathR, filt.rev=paste0("filtered_",pathR),
               truncLen=c(240,200), maxEE=2, truncQ=11, maxN=0, rm.phix=TRUE,
               compress=TRUE, verbose=TRUE, multithread=TRUE)
